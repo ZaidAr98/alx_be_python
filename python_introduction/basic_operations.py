cfa48da24d2bde0990edfa3ac1f006f8b8c11907
Zaid-Ar <zaidhamid600@gmail.com>
2024-06-21 16:20:35 +0400

    fixing the first task

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,12 +1,12 @@
-first_number = 10
-second_number = 5
+number1 = 10
+number2 = 5
 
-sum_of_number = first_number + second_number
-print(f"Addition of {first_number} and {second_number} is {sum_of_number}")
+sum_of_number = number1 + number2
+print(f"Addition of {number1} and {number2} is {sum_of_number}")
 
-difference_of_number = first_number - second_number
-print(f"Subtraction of {first_number} and {second_number} is {difference_of_number}")
+difference_of_number = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {difference_of_number}")
 
 
-product_of_number = first_number * second_number
-print(f"Multiplication of {first_number} and {second_number} is {product_of_number}")
+product_of_number = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {product_of_number}")
