9c3fcfdbc397be067a0340e64b9ca600d8ab3b74
Zaid-Ar <zaidhamid600@gmail.com>
2024-06-21 16:17:13 +0400

    finish first task

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+first_number = 10
+second_number = 5
+
+sum_of_number = first_number + second_number
+print(f"Addition of {first_number} and {second_number} is {sum_of_number}")
+
+difference_of_number = first_number - second_number
+print(f"Subtraction of {first_number} and {second_number} is {difference_of_number}")
+
+
+product_of_number = first_number * second_number
+print(f"Multiplication of {first_number} and {second_number} is {product_of_number}")
