0ea8bb28944c467aaa616223a4517a5cdb0f4c82
Zaid-Ar <zaidhamid600@gmail.com>
2024-06-28 17:50:40 +0400

    fixing pattern

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,8 +1,8 @@
 size = int(input("Enter the size of the pattern:"))
 row = size
 
-while(size>0):
-    for x in range(1,row+1):
+while(row>0):
+    for x in range(1,size+1):
         print("*", end="")
     print()
-    size -= 1 
+    row -= 1 
