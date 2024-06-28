da076ad4696d063333c79ca25ec75f7c9f445e40
Zaid-Ar <zaidhamid600@gmail.com>
2024-06-28 19:10:13 +0400

    fixing reminder 2

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,7 +2,6 @@ task  = str(input("Enter your task: "))
 Priority  = str(input("Priority (high/medium/low): "))
 time_bound = str(input("Is it time-bound? (yes/no): "))
 
-Reminder = f"{task} is a {Priority} priority task"
 match Priority :
     case "high":
               Reminder = f"{task} is a {Priority} priority task"
