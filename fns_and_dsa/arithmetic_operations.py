79e76ab57e0afd57dd32e0e4ce36693b6e836f23
Zaid-Ar <zaidhamid600@gmail.com>
2024-07-06 18:22:19 +0400

    fix 2

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,16 @@
 def perform_operation(num1, num2, operation):
+    result = None 
     match operation:
         case "add":
-            return num1 + num2
+            result = num1 + num2
         case " subtract":
-            return num1 - num2
+            result= num1 - num2
         case "multiply":
-            return num1 * num2
+            result= num1 * num2
         case "divide":
             if(num2 == 0):
-                return "can't divide by zero"
+                result= "can't divide by zero"
             else:
-                return num1 / num2
+                result= num1 / num2
+
+    return result     
